852c010ddf581aca367176dfe6df330381ffc77c
ertanuzunoglu <ertan.uzunoglu@kns.com.tr>
2023-07-24 17:36:09 +0300

    kategory crud işlemleri eklendi

--- routes/admin.js
+++ routes/admin.js
@@ -3,24 +3,16 @@ const router = express.Router();
 const Blog = require("../models/blog");
 const Category = require("../models/category");
 
-router.get("/blogs/delete/:blogid", async function (req, res) {
-	const id = req.params.blogid;
-	try {
-		const blog = await Blog.findOne({ _id: id }).lean();
-		res.render("admin/blog-delete", {
-			title: blog.baslik,
-			blog: blog,
-		});
-	} catch (error) {
-		console.log(error);
-	}
-});
 
-router.post("/blogs/delete/:blogid", async function (req, res) {
-	const id = req.params.blogid;
+router.get("/blogs", async function (req, res) {
 	try {
-		await Blog.findOneAndDelete({ _id: id });
-		res.redirect("/admin/blogs?action=delete");
+		const title = "Blogs";
+		const blogs = await Blog.find().select("baslik resim _id").lean();
+		res.render("admin/blog-list", {
+			blogs: blogs,
+			title: title,
+			action: req.query.action,
+		});
 	} catch (error) {
 		console.log(error);
 	}
@@ -56,7 +48,7 @@ router.post("/blog/create", async function (req, res) {
 	}
 });
 
-router.get("/blogs/:blogid", async function (req, res) {
+router.get("/blog/:blogid", async function (req, res) {
 	const id = req.params.blogid;
 	const blog = await Blog.findOne({ _id: id });
 	const categories = await Category.find();
@@ -68,7 +60,7 @@ router.get("/blogs/:blogid", async function (req, res) {
 	});
 });
 
-router.post("/blogs/:blogid", async function (req, res) {
+router.post("/blog/:blogid", async function (req, res) {
 	const id = req.params.blogid;
 	console.log(req.body);
 	const update = {
@@ -88,15 +80,104 @@ router.post("/blogs/:blogid", async function (req, res) {
 	}
 });
 
-router.get("/blogs", async function (req, res) {
+router.get("/blog/delete/:blogid", async function (req, res) {
+	const id = req.params.blogid;
 	try {
-		const title = "Title";
-		const blogs = await Blog.find().select("baslik resim _id").lean();
-		res.render("admin/blog-list", {
-			blogs: blogs,
+		const blog = await Blog.findOne({ _id: id }).lean();
+		res.render("admin/blog-delete", {
+			title: blog.baslik,
+			blog: blog,
+		});
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+router.post("/blog/delete/:blogid", async function (req, res) {
+	const id = req.params.blogid;
+	try {
+		await Blog.findOneAndDelete({ _id: id });
+		res.redirect("/admin/blogs?action=delete");
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+router.get("/categories", async function (req, res) {
+	try {
+		const title = "Kategoriler";
+		const categories = await Category.find().lean();
+		res.render("admin/category-list", {
+			categories: categories,
 			title: title,
 			action: req.query.action,
 		});
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+router.get("/category/create", async function (req, res) {
+	try {
+		res.render("admin/category-create", {
+			title: "Yeni Kategori",
+		});
+	} catch (error) {}
+});
+
+router.post("/category/create", async function (req, res) {
+	const name = req.body.name;
+	try {
+		await Category.create({
+			name: name,
+		});
+		res.redirect("/admin/categories?action=create");
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+router.get("/category/:categoryid", async function (req, res) {
+	const id = req.params.categoryid;
+	const category = await Category.findOne({ _id: id }).lean();
+	const title = category.name;
+	res.render("admin/category-edit", {
+		category: category,
+		title: title,
+	});
+});
+
+router.post("/category/:categoryid", async function (req, res) {
+	const id = req.params.categoryid;
+	const update = {
+		name: req.body.name,
+	};
+	try {
+		await Category.findOneAndUpdate({ _id: id }, update);
+		res.redirect("/admin/categories?action=edit");
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+router.get("/category/delete/:categoryid", async function (req, res) {
+	const id = req.params.categoryid;
+	try {
+		const category = await Category.findOne({ _id: id }).lean();
+		res.render("admin/category-delete", {
+			title: category.name,
+			category: category,
+		});
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+router.post("/category/delete/:categoryid", async function (req, res) {
+	const id = req.params.categoryid;
+	try {
+		await Category.findOneAndDelete({ _id: id });
+		res.redirect("/admin/categories?action=delete");
 	} catch (error) {
 		console.log(error);
 	}
